4bd179dd907447a4a06985ce2786ca505402e1d3
AnuragMatolia <anuragmatolia@gmail.com>
2026-06-03 09:03:16 +0530

    Add files via upload
    My first assignment

---
+++ myasignment_1.py
@@ -0,0 +1,66 @@
+#Question 1-
+# (a) int-: Stores integer which take 4 Byte
+num=42
+print(type(num))
+# (b) float-: Stores decimal which take 4 Byte
+num1=4.6
+print(type(num1))
+# (c) String-: Sequence of characters
+Name="Anurag"
+print(type(Name))
+# (d) Boolean-: Stores Boolean value which is True/False
+bool1=True
+print(type(bool1))
+
+# Question 2-
+name="Anurag"
+age=19
+city="Pilani"
+print("Name:",name)
+print("Age:",age)
+print("City:",city)
+
+# Question 3-
+user=input("Enter your Name:")
+up_user=user.upper()
+print("Name in Upper Case:",up_user)
+print("Total character:",len(user))
+
+# Question 4-
+Sample="Hello world"
+print("Upper:",Sample.upper())
+print("Title:",Sample.title())
+print("Count of l:",Sample.count("l"))
+print("Index of w:",Sample.index("w"))
+Sample=Sample.replace("world","Guys")
+print("String after Replace:",Sample)
+
+# Question 5-
+Fruit=["Apple","Banana","Mango","Grapes","Pineapple"]
+print(Fruit)
+print("First:",Fruit[0],"& Last:",Fruit[-1])
+print("Length of List:",len(Fruit))
+
+# Question 6-
+lst=[10, 20, 30, 40, 50]
+lst.append(60)
+lst.remove(20)
+print(lst)
+
+# Question 7-
+# AI is the technology that enable Machine to think, learn,make decisions and behave like humans.
+# Importance:-
+# ->Save a lot of time and effort
+# ->Automates repetitive tasks & helps in better decision-making.
+# Uses:-
+# ->ChatGPT
+# ->Google Maps
+# ->Voice Assistants
+# ->Finance Management
+
+# Question 8-
+# (a) ChatGPT- AI as we already know
+# (b) Google Maps Route Prediction- It analyzes traffic and suggests the best route
+# (c) Calculator- not AI
+# (d) Netflix Recommendations- It suggests movies based on user preferences and viewing history.
+# (e) Voice Assistants (Alexa/Siri)- They understand voice commands and respond intelligently.
